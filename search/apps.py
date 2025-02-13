339087120ed9b5f106ef0ffe1a7253c3da2c9965
dipesh7002 <077bct027.dipesh@pcampus.edu.np>
2025-02-13 17:46:22 +0545

    Fixed an issue

--- search/apps.py
+++ search/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class App1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app1'
+    name = 'search'
     
--- search/migrations/0002_app1student_authgroup_authgrouppermissions_and_more.py
+++ search/migrations/0002_app1student_authgroup_authgrouppermissions_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app1', '0001_initial'),
+        ('search', '0001_initial'),
     ]
 
     operations = [
